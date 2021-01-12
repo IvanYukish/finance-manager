c690d9aa6e7c7b47f0cae63d75945a4a39b3b14b
ivanYukish <iwan.jukisch@gmail.com>
2021-01-12 16:39:20 +0200

    added settings for prod environment (docker compose, nginx etc.)

--- src/finance_manager/urls.py
+++ src/finance_manager/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
     path('user/', include('user.urls'))
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
