6464b86d74cfcc8b35473f8a3561c8a466b84ecc
ivanYukish <iwan.jukisch@gmail.com>
2021-02-11 00:49:24 +0200

    reformat test files, changed id to uuid, add new backend for django admin auth.

--- src/finance_manager/settings/base.py
+++ src/finance_manager/settings/base.py
@@ -109,6 +109,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = ['user.backends.EmailBackend']
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
--- src/user/models.py
+++ src/user/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
@@ -7,10 +9,14 @@ from user.validators import phone_validator
 
 
 class CustomUser(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     avatar = models.ImageField(_('Аватар'), upload_to=path_and_rename, blank=True, null=True, default=None)
     phone_number = models.CharField(_('Номер Телефону'), max_length=20, validators=[phone_validator], null=True,
                                     blank=True)
     email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _('Користувач')
