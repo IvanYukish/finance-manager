2ad07a17b247602ca4f942f036da60224f95bb20
ivanYukish <iwan.jukisch@gmail.com>
2021-08-11 15:06:06 +0300

    added profile features(user update by ajax) restructure base templates.

---
+++ src/static/js/custom/profile_update.js
@@ -0,0 +1,37 @@
+$("#profileUpdateForm").submit(function (event) {
+
+    // Stop form from submitting normally
+    event.preventDefault();
+
+    // Get some values from elements on the page:
+    let $form = $(this),
+        url = $form.attr("action");
+
+    // Send the data using post
+    $.ajax(
+        {
+            url: url,
+            type: "POST",
+            data: $form.serializeArray(),
+            dataType: "json",
+            success: function (response) {
+                Swal.fire(
+                    'User',
+                    'was updated successfully',
+                    'success'
+                )
+            },
+            error: function (response) {
+                //TODO show errors
+                let errors = response.responseJSON.errors
+                for (const errorsKey in errors) {
+                    if (errors.hasOwnProperty(errorsKey)) {
+
+                        let key = errors[errorsKey]
+                        console.log(key)
+                    }
+                }
+            }
+        }
+    );
+});
--- src/user/views.py
+++ src/user/views.py
@@ -1,4 +1,8 @@
+from http.client import BAD_REQUEST, OK
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 
@@ -34,13 +38,34 @@ class FriendDeleteView(LoginRequiredMixin, DeleteView):
         pass
 
 
-class ProfileDetailView(LoginRequiredMixin, DetailView):
+class ProfileDetailView(LoginRequiredMixin, DetailView, UpdateView):
     model = CustomUser
+    form_class = ProfileUpdateForm
     template_name = 'user/profile_detail.html'
     context_object_name = 'user'
+    success_url = reverse_lazy('profile')
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        if request.is_ajax():
+            form = self.get_form()
+            if form.is_valid():
+                self.form_valid(form)
+                return JsonResponse({'status': 'success'}, status=OK)
+            else:
+                errors = form.errors
+                return JsonResponse({"errors": errors}, status=BAD_REQUEST, json_dumps_params={'ensure_ascii': False})
+
+        return render(request, self.template_name, self.get_form())
 
     def get_object(self, queryset=None):
         return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        context['form'] = ProfileUpdateForm(instance=self.get_object())
+        return context
 
 
 class ProfileUpdateView(LoginRequiredMixin, UpdateView):
